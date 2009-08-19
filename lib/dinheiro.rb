35673fb49cf9e67fe84afe20db69b05e1576bfef
Cleiton Francisco <cleitonfco@gmail.com>
2009-08-19 09:51:18 -0300

    Aperfeiçoando

--- lib/dinheiro.rb
+++ lib/dinheiro.rb
@@ -1,32 +1,67 @@
 class Dinheiro
 
-  MOEDAS = {
-    "USD" => { :rate => 1.4101, :format => "US$" }, 
-    "BRL" => { :rate => 2.6281, :format => "R$" }, 
-    "EUR" => { :rate => 1, :format => "€" }
-  }
-  MOEDA_REFERENCIA = "EUR"
   attr_reader :valor, :moeda_base
+  CONFIGURACAO_MOEDAS = {
+    'USD' => { :nome => 'dólar americano', :prefixo => '$ ',  :separador_decimal => '.', :separador_milhar => ',' },
+    'EUR' => { :nome => 'euro',            :prefixo => '€ ',  :separador_decimal => ',', :separador_milhar => '.' },
+    'BRL' => { :nome => 'real',            :prefixo => 'R$ ', :separador_decimal => ',', :separador_milhar => '.' }
+  }
+  COTACOES = {'USD' => 1.4101, 'BRL' => 2.6281, 'EUR' => 1 }
+  MOEDA_REFERENCIA = 'EUR'
 
-  def initialize(valor = 0, moeda_base = "EUR")
-    @valor = valor
+  def initialize(valor = 0, moeda_base = 'EUR')
+    @valor = (valor * 100).to_i / 100.0
     @moeda_base = moeda_base
   end
 
   def para(moeda_destino)
-    return @valor * MOEDAS[moeda_destino][:rate] if @moeda_base == MOEDA_REFERENCIA
+    return @valor * COTACOES[moeda_destino] if @moeda_base == MOEDA_REFERENCIA
     if moeda_destino == MOEDA_REFERENCIA
-      @valor / MOEDAS[moeda_destino][:rate]
+      @valor / COTACOES[@moeda_base]
     else
-      @valor / MOEDAS[MOEDA_REFERENCIA][:rate] * MOEDAS[moeda_destino][:rate]
+      @valor / COTACOES[@moeda_base] * COTACOES[moeda_destino]
     end
   end
 
   def formatado
-    "#{MOEDAS[@moeda_base][:format]} #{@valor.to_f}"
+    "#{CONFIGURACAO_MOEDAS[@moeda_base][:prefixo]}#{to_s}"
   end
 
   def to_s
-    @valor.to_f.to_s
+    separadores = (inteiro.to_s.length / 3.0).ceil - 1
+    saldo = inteiro.to_s.length % 3
+    re = '^([+-]?)'
+    re << '(\d{' + saldo.to_s + '})' if saldo > 0
+    re << '(\d{3})' * separadores
+    re << '$'
+    rex = Regexp.new(re)
+
+#http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote;date=20090814;currency=true?view=basic&format=json
+#    puts rex.source
+#    puts CONFIGURACAO_MOEDAS[@moeda_base][:separador_decimal]
+    valor = inteiro.to_s.gsub(rex, "#{$1}#{$2},#{$3}")
+    valor + CONFIGURACAO_MOEDAS[@moeda_base][:separador_decimal] + decimal.to_s
+#    valor = @valor.to_s.gsub(".", CONFIGURACAO_MOEDAS[:separador_decimal])
+#    if /^([+-]?)(\d)$/ =~
+#      return "#{$1}0#{CONFIGURACAO_MOEDAS[:separador_decimal]}0#{$2}"
+#    end
+#    /^([+-]?)(\d*)(\d\d)$/ =~ quantia.to_s
+#    "#{$1}#{$2.to_i}.#{$3}"
+#    return inteiro if quantidade_de_passos(inteiro) == 0
+#    resultado = ""
+#    quantidade_de_passos(inteiro).times do |passo|
+#      resultado = "." + inteiro[-QUANTIDADE_DIGITOS + passo * -QUANTIDADE_DIGITOS, QUANTIDADE_DIGITOS] + resultado
+#    end
+#    resultado = inteiro[0, digitos_que_sobraram(inteiro)] + resultado
+#    resultado.gsub(/^(-?)\./, '\1')
   end
+
+  private
+    def inteiro
+      @valor.to_i
+    end
+    
+    def decimal
+      ((@valor - inteiro) * 100).to_i
+    end
 end
--- spec/dinheiro_spec.rb
+++ spec/dinheiro_spec.rb
@@ -2,49 +2,66 @@ require 'lib/dinheiro.rb'
 
 describe Dinheiro do
   before(:each) do
-    @cotacao = Dinheiro::MOEDAS
+    @cotacao = Dinheiro::COTACOES
+    @valor_original = 8177.8995
+    @valor_moeda = (@valor_original * 100).to_i / 100.0
   end
 
   describe "em Real" do
     before(:each) do
-      valor = rand(500) / 100
-      @dinheiro = Dinheiro.new(valor, 'BRL')
+      @dinheiro = Dinheiro.new(@valor_original, 'BRL')
     end
 
     it "convertendo para Euro" do
-      @dinheiro.para('EUR').should == @dinheiro.valor * @cotacao['EUR'][:rate]
+      @dinheiro.para('EUR').should == @valor_moeda / @cotacao['BRL']
     end
 
     it "convertendo para Dólar" do
-      @dinheiro.para('USD').should == @dinheiro.valor / @cotacao['EUR'][:rate] * @cotacao['USD'][:rate]
+      @dinheiro.para('USD').should == @valor_moeda / @cotacao['BRL'] * @cotacao['USD']
     end
 
-    it "formatado deve retornar o símbolo da moeda" do
-      @dinheiro.formatado.should == "R$ #{@dinheiro.valor.to_f}"
+    it "formatado deve retornar o formato correto da moeda" do
+      @dinheiro.formatado.should == "R$ 8.177,89"
     end
 
-    it "formatado deve retornar o símbolo da moeda ----" do
-      @dinheiro.to_s.should == @dinheiro.valor.to_f.to_s
+    it "to_s deve estar de acordo com o padrão da moeda" do
+      @dinheiro.to_s.should == "8.177,89"
     end
   end
 
   describe "em Dólar" do
     before(:each) do
-      valor = rand(500) / 100
-      @dinheiro = Dinheiro.new(valor, 'USD')
+      @dinheiro = Dinheiro.new(@valor_original, 'USD')
     end
 
     it "convertendo para Euro" do
-      @dinheiro.para('EUR').should == @dinheiro.valor * @cotacao['EUR'][:rate]
+      @dinheiro.para('EUR').should == @valor_moeda / @cotacao['USD']
     end
 
     it "convertendo para Real" do
-      @dinheiro.para('BRL').should == @dinheiro.valor / @cotacao['EUR'][:rate] * @cotacao['BRL'][:rate]
+      @dinheiro.para('BRL').should == @valor_moeda / @cotacao['USD'] * @cotacao['BRL']
     end
 
-    it "formatado deve retornar o símbolo da moeda" do
-      @dinheiro.formatado.should == "US$ #{@dinheiro.valor.to_f}"
+    it "formatado deve retornar o formato correto da moeda" do
+      @dinheiro.formatado.should == "$ 8,177.89"
     end
   end
 
+  describe "em Euro" do
+    before(:each) do
+      @dinheiro = Dinheiro.new(@valor_original, 'EUR')
+    end
+
+    it "convertendo para Real" do
+      @dinheiro.para('BRL').should == @valor_moeda * @cotacao['BRL']
+    end
+
+    it "convertendo para Dólar" do
+      @dinheiro.para('USD').should == @valor_moeda * @cotacao['USD']
+    end
+
+    it "formatado deve retornar o formato correto da moeda" do
+      @dinheiro.formatado.should == "€ 8.177,89"
+    end
+  end
 end
